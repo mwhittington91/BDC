6e8df50cdeb34a15e85f8106e2ced8fb3c8eadb6
Michael W <mwhittington@ncta.com>
2024-06-04 17:05:25 -0400

    chore: Remove unused code and update function signature in bdc.py

--- bdc_api.py
+++ bdc_api.py
@@ -51,45 +51,7 @@ async def getDownloadFile(file_id: int):
     reqUrl = f"{baseURL}api/public/map/downloads/downloadfile/availability/{file_id}"
     response = await client.get(url=reqUrl, headers=headersList)
 
-    zip_path = f"/Users/mwhittington/Documents/Code/BDC/{file_id}.csv.zip"
-    extract_path = "/Users/mwhittington/Documents/Code/BDC/exports"
-
-    # Save the content to a file
-    with open(zip_path, "wb") as file:
-        file.write(response.content)
-
-    # Extract the zip file
-    with zipfile.ZipFile(zip_path, "r") as zip_ref:
-        zip_ref.extractall(extract_path)
-
-    # delete the zip file
-    os.remove(path=zip_path)
-
-    return f"File {file_id} has been downloaded and extracted"
-
-
-# combine all the files into one Sqlite database
-def combineFilesintoDB(
-    extract_path: str = "./exports", db_path: str = "./.db", table_name: str = "Table 1"
-):
-    # combined_path = "/Users/mwhittington/Documents/Code/BDC/combined.csv"
-    frames = []
-
-    for file in os.listdir(extract_path):
-        # Load the CSV files into DataFrames
-        df = pd.read_csv(f"{extract_path}/{file}", encoding="utf-8")
-        frames.append(df)
-
-    # Combine the DataFrames
-    combined_df = pd.concat(frames, ignore_index=True)
-
-    conn = sqlite3.connect(db_path)
-
-    combined_df.to_sql(table_name, conn, if_exists="replace", index=False)
-
-    conn.close()
-
-    return "Files have been combined"
+    return response
 
 
 async def downloadFiles(files: list):
@@ -107,9 +69,3 @@ async def downloadFiles(files: list):
     print(f"Missing files: {missing}")
 
 
-async def main():
-    pass
-
-
-if __name__ == "__main__":
-    print(asyncio.run(getDownloadList()))
---
+++ utils.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+import sqlite3
+import zipfile
+
+def combineFilesintoDB(
+    extract_path: str = "./exports", db_path: str = "./.db", table_name: str = "Table 1"
+):
+    # combined_path = "/Users/mwhittington/Documents/Code/BDC/combined.csv"
+    frames = []
+
+    for file in os.listdir(extract_path):
+        # Load the CSV files into DataFrames
+        df = pd.read_csv(f"{extract_path}/{file}", encoding="utf-8")
+        frames.append(df)
+
+    # Combine the DataFrames
+    combined_df = pd.concat(frames, ignore_index=True)
+
+    conn = sqlite3.connect(db_path)
+
+    combined_df.to_sql(table_name, conn, if_exists="replace", index=False)
+
+    conn.close()
+
+    return "Files have been combined"
+
+def extractZip(response, file_id):
+    
+    zip_path = f"/Users/mwhittington/Documents/Code/BDC/{file_id}.csv.zip"
+    extract_path = "/Users/mwhittington/Documents/Code/BDC/exports"
+
+    # Save the content to a file
+    with open(zip_path, "wb") as file:
+        file.write(response.content)
+
+    # Extract the zip file
+    with zipfile.ZipFile(zip_path, "r") as zip_ref:
+        zip_ref.extractall(extract_path)
+
+    # delete the zip file
+    os.remove(path=zip_path)
